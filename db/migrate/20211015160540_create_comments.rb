c1b5df97df18ac5ea8b82f67d3c80019a428aae2
sakaki38 <h.sakaki38@gmail.com>
2021-10-24 18:48:40 +0900

    詳細修正３

--- db/migrate/20211015160540_create_comments.rb
+++ db/migrate/20211015160540_create_comments.rb
@@ -1,8 +1,6 @@
 class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
-      t.integer :user_id
-      t.integer :prototype_id
       t.timestamps
       t.text :content, null: false
       t.references :prototype, null: false, foreign_key: true
